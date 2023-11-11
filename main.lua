30ef5db96ee27b34ee88f0eb338e076e93c9729c
BuildTools <unconfigured@null.spigotmc.org>
2023-11-11 10:40:17 -0500

    bug fixes

--- main.lua
+++ main.lua
@@ -2,6 +2,7 @@ function love.load()
     love.window.setMode(0, 0, {fullscreentype = 'desktop', fullscreen = 'true'})
     love.graphics.setDefaultFilter('nearest', 'nearest')
     love.graphics.setLineWidth(2.5)
+    love.graphics.setBackgroundColor(1/157, 1/205, 1/216)
 
     require 'math'
 
@@ -15,9 +16,7 @@ function love.load()
      world:addCollisionClass('static')
      world:addCollisionClass('banana')
      world:addCollisionClass('point', {ignores = {'static', 'banana'}})
-     world:setQueryDebugDrawing(true)
      
-
     banana = {}
      banana.x = 0
      banana.y = 0
@@ -34,17 +33,18 @@ function love.load()
      
      banana.collider:setCollisionClass('banana')
      banana.collider:setAngle(math.pi / 2)
+     banana.collider:setPosition(-12, -5)
      
-     
+     joystick = nil
      force = 0
      angle = 0
      held = false
      b = 0
      c = 0
-    
 
+    
     sti = require 'libraries/Simple-Tiled-Implementation-master/sti'
-     gameMap = sti('maps/untitled.lua')
+     gameMap = sti('maps/map.lua')
 
      if gameMap.layers['static'] then
         for i, obj in pairs(gameMap.layers['static'].objects) do
@@ -54,25 +54,32 @@ function love.load()
         end
     end
 
-    banana.collider:applyLinearImpulse(0, -100)
     
+    
+    cam:lookAt(banana.collider:getX(), banana.collider:getY())
 end
 
 function love.update(dt)
+    
     banana.x, banana.y = banana.collider:getPosition()
     force = force + dt
     banana.rotation = banana.collider:getAngle()
     --cam:lookAt(banana.x, banana.y)
-    cam:lockPosition(banana.x, banana.y, cam.smooth.damped(2))
+    cam:lockPosition(math.floor(banana.x), math.floor(banana.y), cam.smooth.damped(2))
     world:update(dt)
     banana.x, banana.y = banana.collider:getPosition()
 end
 
 function love.draw()
+    love.graphics.setColor(1/157, 1/205, 1/216)
+    love.graphics.rectangle('fill', 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
+    love.graphics.setColor(1, 1, 1, 1)
     cam:attach()
+        gameMap:drawTileLayer(gameMap.layers['sky'])
+        gameMap:drawTileLayer(gameMap.layers['Tile Layer 1'])
         love.graphics.draw(banana.sprite, banana.x , banana.y , banana.rotation, 1, 1, banana.sprite:getWidth() / 2, banana.sprite:getHeight() / 2)
         love.graphics.setLineWidth(.5)
-        world:draw()
+        
         if held == true then
             mx, my = cam:mousePosition()
             w = mx - banana.x 
@@ -87,27 +94,29 @@ function love.draw()
             love.graphics.line(banana.x, banana.y - 15, banana.x + nw * force * 15, banana.y - 15 + nh * force * 15)
         end
     cam:detach()
-
-    love.graphics.print(tostring(force))
 end
 
 function love.mousepressed(x, y, button, istouch, presses)
-    force = 0
-    held = true
+    if button == 1 then
+        force = 0
+        held = true
+    end
 end
 
 function love.mousereleased(x, y, button, istouch, presses)
-    mx, my = cam:mousePosition()
-    w = mx - banana.x 
-    h = my - banana.y
-    hyp = math.sqrt(w*w + h*h)
-    nw = w / hyp 
-    nh = h / hyp
-    if force > .8 then
+    if button == 1 then
+        mx, my = cam:mousePosition()
+        w = mx - banana.x 
+        h = my - banana.y
+        hyp = math.sqrt(w*w + h*h)
+        nw = w / hyp 
+        nh = h / hyp
+        if force > .8 then
         force = .8
-    end
-    if #world:queryRectangleArea(banana.x - 15, banana.y, 30, 15) > 1 then  
+        end
+        if #world:queryRectangleArea(banana.x - 15, banana.y, 30, 15) > 1 then  
         banana.collider:applyLinearImpulse(nw * force * 400, nh * force * 400)
         held = false
+        end
     end
 end
